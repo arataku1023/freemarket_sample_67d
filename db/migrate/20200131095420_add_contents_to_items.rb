70db087e6384d9d8c3c03603d91c537df3a2ca05
kokeshi1357 <kokesi1357@icloud.com>
2020-02-02 18:44:42 +0900

    category

--- db/migrate/20200131095420_add_contents_to_items.rb
+++
@@ -1,8 +0,0 @@
-class AddContentsToItems < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :name, :string
-    add_index :items, :name
-    add_column :items, :mail, :string
-    add_column :items, :mail_way, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_095420) do
+ActiveRecord::Schema.define(version: 2020_02_02_075510) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
