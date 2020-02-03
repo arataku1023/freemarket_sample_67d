76af987f62692f1ca6da9f7c39a342726f364c75
n-domi <59164751+n-domi@users.noreply.github.com>
2020-02-03 16:09:56 +0900

    2020/02/03/16:00

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'items/new'
+  # get 'items/confirm'
   devise_for :users
   root 'home#index'
   resources :users,only: [:index,:show,:edit,:create]
-  resources :items
+  resources :items do
+    member do
+      get 'confirm'
+    end
+  end  
   resources :categories, only: [:index]
 end
 
