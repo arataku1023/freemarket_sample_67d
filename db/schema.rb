2c0b3a8e7a2889b30a5cf6987d7bb35dc5cf508e
jyunpeimamiya <jyunpei0225@yahoo.com>
2020-02-04 16:53:45 +0900

    log-in-wezard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_095225) do
+ActiveRecord::Schema.define(version: 2020_02_04_060624) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.string "prefecture", null: false
+    t.string "town", null: false
+    t.string "address_num", null: false
+    t.string "apartment_info"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_addresses_on_users_id"
+  end
+
+  create_table "creditcards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
@@ -33,4 +51,5 @@ ActiveRecord::Schema.define(version: 2020_01_29_095225) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users", column: "users_id"
 end
