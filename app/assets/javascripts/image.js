a907cd0c3fce7bee33b920007b2c7c61f20b806a
n-domi <59164751+n-domi@users.noreply.github.com>
2020-02-05 11:34:32 +0900

    202002051130

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,11 +1,36 @@
-$(function(){
- $('#1').mouseover(function(){
-   $('.relative').css('left', '0'),$(this).css('opacity','1'),$('#2').css('opacity','0.6'),$('#3').css('opacity','0.6');
- }),
- $('#2').mouseover(function(){
-   $('.relative').css('left', '-620px'),$(this).css('opacity','1'),$('#3').css('opacity','0.6'),$('#1').css('opacity','0.6');
- }),
- $('#3').mouseover(function(){
-   $('.relative').css('left', '-1240px'),$(this).css('opacity','1'),$('#2').css('opacity','0.6'),$('#1').css('opacity','0.6');
- });
-});
+$(function() {
+
+  $('#0').hover(
+    function() {
+      $('.relative').css('left', '0'),$('#1').addClass('opa'),$('#2').addClass('opa');
+    },
+    function() {
+      $('.relative').css('left', '0');
+    }
+  );
+
+  $('#1').hover(
+    function() {
+      $('.relative').css('left', '-620px'),$(this).removeClass('opa').addClass('active'),$('#0').addClass('opa');
+    },
+    function() {
+      $('.relative').css('left', '0'),$(this).addClass('opa').removeClass('active'),$('#0').removeClass('opa');
+    }
+  );
+
+  $("#2").hover(
+    function() {
+      $('.relative').css('left', '-1240px'),$(this).removeClass('opa'),$(this).addClass('active'),$('#0').addClass('opa');
+    },
+    function() {
+      $('.relative').css('left', '0'),$(this).addClass('opa').removeClass('active'),$('#0').removeClass('opa');
+    }
+  );
+})
+
+
+
+
+
+
+ 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,13 @@ class ItemsController < ApplicationController
   end
   
   def show
+    @item = Item.find_by(id: params[:id])
+    @images = Image.where(item_id: @item.id)
+    @brand = Brand.find_by(id: @item.brand_id)
+    @user = User.find_by(id: @item.user_id)
+    @grandchildren = Category.find_by(id: @item.category_id)
+    @children = @grandchildren.parent
+    @parent = @children.parent  
   end
 
   def confirm
