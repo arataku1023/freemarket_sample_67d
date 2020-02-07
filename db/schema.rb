4332fb1dbed51da942a7f0d7ca68ad01a3f1fefc
kokeshi1357 <kokesi1357@icloud.com>
2020-02-07 16:52:29 +0900

    migration-sass

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2020_02_04_162302) do
+ActiveRecord::Schema.define(version: 2020_02_07_065600) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
@@ -27,15 +25,8 @@ ActiveRecord::Schema.define(version: 2020_02_04_162302) do
     t.index ["users_id"], name: "index_addresses_on_users_id"
   end
 
-  create_table "birth_years", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -54,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_162302) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
+    t.text "image", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -66,24 +57,21 @@ ActiveRecord::Schema.define(version: 2020_02_04_162302) do
     t.text "detail", null: false
     t.integer "price", null: false
     t.integer "status", limit: 1, null: false
-
+    t.string "region", null: false
+    t.string "arrival_date", null: false
     t.string "mail", null: false
     t.string "mail_way", null: false
-
-
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.bigint "brand_id"
+    t.integer "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.integer "prefecture_id"
     t.integer "arrival_date_id"
-
-
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["name"], name: "index_items_on_name"
+    t.index ["mail"], name: "index_items_on_mail"
+    t.index ["mail_way"], name: "index_items_on_mail_way"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -98,7 +86,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_162302) do
     t.string "birth_year", default: "", null: false
     t.string "birth_month", default: "", null: false
     t.string "birth_day", default: "", null: false
-    t.string "image", default: ""
+    t.text "image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -109,4 +97,9 @@ ActiveRecord::Schema.define(version: 2020_02_04_162302) do
   end
 
   add_foreign_key "addresses", "users", column: "users_id"
+  add_foreign_key "cards", "users"
+  add_foreign_key "images", "items"
+  add_foreign_key "items", "brands"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
