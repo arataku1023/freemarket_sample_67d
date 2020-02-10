d3924eed9abbb2afb91e187293700125c3cac954
n-domi <59164751+n-domi@users.noreply.github.com>
2020-02-10 10:58:35 +0900

    0210

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   def new
    @item = Item.new
-   @item.images.new
+   @item.images.build
    @category_parent_array = ["---"]
    Category.where(ancestry: nil).each do |parent|   #データベースから、親カテゴリーのみ抽出し、配列化
      @category_parent_array << parent.name
@@ -15,14 +15,32 @@ class ItemsController < ApplicationController
 
 
   def create
-    # binding.pry
-    @item = Item.create!(item_params)
+    @item = Item.new(item_params)
+    @item.save
     # @item.save
-   #  Image.create
+    # Image.create
     # Image.create(image_params)
     redirect_to "/"
    end
-  
+
+   def edit
+    @item = Item.find(params[:id])
+    @item.images.build
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|   #データベースから、親カテゴリーのみ抽出し、配列化
+      @category_parent_array << parent.name
+    end
+    respond_to do |format|
+      format.html
+      format.json
+    end
+   end
+
+   def update
+    @item = item.find(params[:id])
+    @item.update(item_params)
+  end
+
   def get_category_children
    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
@@ -54,7 +72,10 @@ class ItemsController < ApplicationController
 
   private
   def item_params   #後でmerge内を追加...brand_id etc. #category_id:
-    params.require(:item).permit(:name, :detail, :price, :status, :arrival_date_id, :mail, :mail_way, :prefecture_id, :category_id).merge(user_id: "1",brand_id: "1")
+    # params.require(:item).permit(:name, :detail, :price, :status, :arrival_date_id, :mail, :mail_way, :prefecture_id, :category_id).merge(user_id: "1", brand_id: "1")
+    params.require(:item).permit(:name, :detail, :price, :status, :arrival_date_id, :mail, :mail_way, :prefecture_id, :category_id, images_attributes: [:image]).merge(user_id: "1", brand_id: "1")
+
+    # params.require(:image).permit(:image, :image_id)
   end
 
 
