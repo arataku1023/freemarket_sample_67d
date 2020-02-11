6639c8ada78732fd03a30383a4bbebe87fb141cd
arataku1023 <59153438+arataku1023@users.noreply.github.com>
2020-02-11 13:59:14 +0900

    edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -73,7 +73,7 @@ class ItemsController < ApplicationController
   private
   def item_params   #後でmerge内を追加...brand_id etc. #category_id:
     # params.require(:item).permit(:name, :detail, :price, :status, :arrival_date_id, :mail, :mail_way, :prefecture_id, :category_id).merge(user_id: "1", brand_id: "1")
-    params.require(:item).permit(:name, :detail, :price, :status, :arrival_date_id, :mail, :mail_way, :prefecture_id, :category_id, images_attributes: [:image]).merge(user_id: "1")
+    params.require(:item).permit(:name, :detail, :price, :status, :arrival_date_id, :mail, :mail_way, :prefecture_id, :category_id, images_attributes: [:image]).merge(user_id: current_user.id)
 
     # params.require(:image).permit(:image, :image_id)
   end
