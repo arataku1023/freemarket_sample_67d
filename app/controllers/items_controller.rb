623bfbd04f6fe9f341972d45f71616aaa20df738
n-domi <59164751+n-domi@users.noreply.github.com>
2020-02-13 13:13:18 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,16 +52,16 @@ class ItemsController < ApplicationController
     end   
   end
 
-  # def get_category_children
-  #   #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-  #   @category_children = Category.find_by(name: "#{params[:parent_name]}").children
-  # end
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}").children
+  end
 
-  # # 子カテゴリーが選択された後に動くアクション
-  # def get_category_grandchildren
-  #   #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-  #   @category_grandchildren = Category.find("#{params[:child_id]}").children
-  # end
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
   
   def show
     @images = Image.where(item_id: @item.id)
