4ab05c013a8eee2561c4b23300f40070f456b2b0
kokeshi1357 <kokesi1357@icloud.com>
2020-02-18 20:37:41 +0900

    k

--- db/migrate/20200213093111_addresses.rb
+++ db/migrate/20200213093111_addresses.rb
@@ -2,7 +2,7 @@ class Addresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
       t.string :post_code, null: false
-      t.integer :prefecture_id, null: false
+      t.integer :prefecture, null: false
       t.string :town, null: false
       t.string :address_num, null: false
       t.string :apartment_info
