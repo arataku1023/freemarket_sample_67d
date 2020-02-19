b96e3c309b156c81e1812d286de363b0351fef23
n-domi <59164751+n-domi@users.noreply.github.com>
2020-02-19 19:49:50 +0900

    お気に入り機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user=User.find(current_user.id)
+    @exhibited_items_count = Item.where(user_id: current_user.id).count
+    @purchased_items_count = Item.where(buyer_id: current_user.id).count
     @favorites = Favorite.where(user_id: current_user.id)
   end
   
