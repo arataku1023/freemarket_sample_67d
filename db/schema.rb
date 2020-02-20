01b70ff4fcbc2a2e5eff08f59d9e0fb12a17374b
jyunpeimamiya <jyunpei0225@yahoo.com>
2020-02-20 20:39:31 +0900

    fix2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_052558) do
+ActiveRecord::Schema.define(version: 2020_02_18_102619) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
-    t.string "prefectures", null: false
     t.string "town", null: false
     t.string "address_num", null: false
     t.string "apartment_info"
@@ -22,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_02_17_052558) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "prefecture"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
@@ -44,15 +44,6 @@ ActiveRecord::Schema.define(version: 2020_02_17_052558) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
-  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_favorites_on_item_id"
-    t.index ["user_id", "item_id"], name: "index_favorites_on_user_id_and_item_id", unique: true
-    t.index ["user_id"], name: "index_favorites_on_user_id"
-    
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
     t.bigint "item_id"
@@ -61,6 +52,16 @@ ActiveRecord::Schema.define(version: 2020_02_17_052558) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_comments_on_item_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_favorites_on_item_id"
+    t.index ["user_id", "item_id"], name: "index_favorites_on_user_id_and_item_id", unique: true
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -117,10 +118,10 @@ ActiveRecord::Schema.define(version: 2020_02_17_052558) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
-  add_foreign_key "favorites", "items"
-  add_foreign_key "favorites", "users"
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "items"
+  add_foreign_key "favorites", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"
